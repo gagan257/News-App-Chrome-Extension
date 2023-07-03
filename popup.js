60280171cd88934d674b6c84d576ace55348bdec
Gagan Arora <arorag2577@gmail.com>
2023-07-03 20:57:39 +0530

    initial working

---
+++ popup.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var newsList = document.getElementById("news-list");
+  
+    fetchNews();
+  
+    function fetchNews() {
+      var apiKey = "a6dd0e2e765b47e9b211cf5fc2f5c8f1";
+      var apiUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + apiKey;
+  
+      fetch(apiUrl)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          displayNews(data.articles);
+        })
+        .catch(function(error) {
+          console.log("Error fetching news:", error);
+        });
+    }
+  
+    function displayNews(articles) {
+      for (var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        var listItem = document.createElement("li");
+        var link = document.createElement("a");
+        link.href = article.url;
+        link.textContent = article.title;
+        listItem.appendChild(link);
+        newsList.appendChild(listItem);
+      }
+    }
+  });
